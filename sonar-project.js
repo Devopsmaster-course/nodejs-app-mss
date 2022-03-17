aa93c0bbe5a2d7c1b49e83ed6e5fa4e40ef94e73
DevOps Ram <98338283+Ramurams098@users.noreply.github.com>
2022-03-18 01:29:45 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.63.115:9000/',
+  serverUrl: 'http://172.31.9.98:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
